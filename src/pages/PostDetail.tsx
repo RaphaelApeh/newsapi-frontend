c41ea5b1835876f4cfa2666346e46e3ce130ac33
raphael apeh <raphaelapeh37@gmail.com>
2025-03-07 22:43:15 +0100

    working on the post detail page

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom"
 import { BASE_API_ENDPOINT } from "../helpers/api"
 import { useEffect, useState } from "react";
 import { type Posts } from "../types";
+import NarBar from "../components/NarBar";
+import Footer from "../components/Footer";
 
 
 export const PostDetail = ()=> {
@@ -47,16 +49,43 @@ export const PostDetail = ()=> {
     if (!loading) return <h1>No Content</h1>
     return (
         <>
-            {loading ?<div>
-                <h1>{page.user.username} {page.id}</h1>
-            <small>
-                <img src={page.image} alt="" />
-                {page.content}
-            </small>
-            <h1>
-                {page.comments?.length}
-            </h1>
-            </div> : "No Content"}
+        <NarBar/>
+            {loading ?
+            <div className="container">
+            <section className="article-page">
+                <article>
+                    <img src={page.image} alt="" />
+                    <h2>{page.title}</h2>
+                    <div>
+                        <p>
+                            <i className="fas fa-user fa-1x"></i> Written By {page.user.username} {page.timestamp}
+                        </p>
+                        <p className="entertainment-category">Entertainment</p>
+                    </div>
+                    <p>{page.content}</p>
+                </article>
+    
+                <article>
+                    <h3>CATEGORIES</h3>
+                    <ul>
+                        <li>Sports</li>
+                        <li>Entertainment</li>
+                        <li>Technology</li>
+                        <li>Fashion</li>
+                        <li>Shopping</li>
+                    </ul>
+                </article>
+    
+                <article>
+                    <h3>JOIN OUR CLUB</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, id?
+                    </p>
+                    <a href="#">Join Now</a>
+                </article>
+            </section>
+        </div> : "No Content"}
+        <Footer/>
         </>
     )
 }
