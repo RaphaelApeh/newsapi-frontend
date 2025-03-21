d9d35f47459210e0b51e7c667d57c02ae248751d
raphael apeh <raphaelapeh37@gmail.com>
2025-03-21 21:21:36 +0100

    working on post update

---
+++ src/helpers/data.ts
@@ -0,0 +1,6 @@
+
+export const STATUS = [
+    {id: 1, name: "Pending"},
+    {id: 2, name: "Draft"},
+    {id: 3, name: "Active"},
+]
--- src/pages/PostUpdate.tsx
+++ src/pages/PostUpdate.tsx
@@ -4,6 +4,9 @@ import { getPost } from '../helpers/api'
 import { useParams } from 'react-router-dom'
 import NarBar from '../components/NarBar'
 import Footer from '../components/Footer'
+import type { PostCreation } from '../types'
+import { useForm } from 'react-hook-form'
+import { STATUS } from '../helpers/data'
 
 export const PostUpdate: React.FC = () => {
   const { slug } = useParams()
@@ -12,27 +15,29 @@ export const PostUpdate: React.FC = () => {
     queryKey: ["updatepost"],
     queryFn: () => getPost(String(slug), 2)
   })
-  
+  const { register, handleSubmit } = useForm<PostCreation>()  
   if (isLoading) return (
     <div className="preloader">
         <div></div>
         <div>Loading....</div>
     </div>
   )
+  const onSubmit = <T,>(data: T) => {
+    console.log(data)
+  }
   return (
     <>
       <NarBar/>
-      <form className='mt-12 mb-12'>
-        <input className='mt-4 form-control' type="text" value={data.title} />
-        <textarea value={data.content} className='mt-4 form-control'></textarea>
-        {/* work on it later */}
-        {/* <select className='mb-4 form-control' >
-                <option value="pending">Pending</option>
-                <option value="active">Active</option>
-                <option value="draft">Draft</option>
-        </select> */}
-        <input type="file" className='mt-4 form-control' />
-        <img src={data.image} width={5} />
+      <form className='mt-12 mb-12' onSubmit={handleSubmit(onSubmit)}>
+        <input className='mt-4 form-control' {...register("title")} type="text" value={data.title} />
+        <textarea {...register("content")} value={data.content} className='mt-4 form-control'></textarea>
+        <select className='mb-4 form-control' {...register("status")}>
+          {STATUS.map(option => (
+            <option key={option.id} value={option.name.toLowerCase()}>{option.name}</option>
+          ))}
+        </select>
+        <input type="file" {...register("image")} className='mt-4 form-control' />
+        {/* <img src={data.image} width={5} /> */}
           <button className='mt-4 btn btn-success'>Update</button>
       </form>
       <Footer/>
