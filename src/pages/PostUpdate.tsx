ca473e22ce410c673d5a5e5ad46773ab52326c8f
raphael apeh <raphaelapeh37@gmail.com>
2025-03-21 21:59:03 +0100

    working on post update

--- src/pages/PostUpdate.tsx
+++ src/pages/PostUpdate.tsx
@@ -1,19 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import React from 'react'
-import { getPost } from '../helpers/api'
-import { useParams } from 'react-router-dom'
+import { getPost, updatePost } from '../helpers/api'
+import { useNavigate, useParams } from 'react-router-dom'
 import NarBar from '../components/NarBar'
 import Footer from '../components/Footer'
-import type { PostCreation } from '../types'
+import type { PostCreation, Posts } from '../types'
 import { useForm } from 'react-hook-form'
 import { STATUS } from '../helpers/data'
 
 export const PostUpdate: React.FC = () => {
   const { slug } = useParams()
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const { data, isLoading } = useQuery({
     queryKey: ["updatepost"],
     queryFn: () => getPost(String(slug), 2)
+  })
+  const mutation = useMutation({
+    mutationFn: (data: Posts) => updatePost(String(slug), data),
+    onSuccess: () => navigate(`posts/${data.slug}`)
   })
   const { register, handleSubmit } = useForm<PostCreation>()  
   if (isLoading) return (
@@ -22,12 +26,24 @@ export const PostUpdate: React.FC = () => {
         <div>Loading....</div>
     </div>
   )
-  const onSubmit = <T,>(data: T) => {
-    console.log(data)
+  const onSubmit: any = (data: Posts) => {
+      const formData = new FormData()
+      formData.append("title", data.title)
+      formData.append("content", data.content)
+      formData.append("image", data.image[0])
+      //@ts-ignore
+      formData.append("status", data.status)
+      //@ts-ignore
+      mutation.mutate(FormData)
   }
   return (
     <>
       <NarBar/>
+      {mutation.isError ? (
+          <div className='alert alert-danger'>
+            <small>{mutation.error?.message}</small>
+          </div>
+      ): ""}
       <form className='mt-12 mb-12' onSubmit={handleSubmit(onSubmit)}>
         <input className='mt-4 form-control' {...register("title")} type="text" value={data.title} />
         <textarea {...register("content")} value={data.content} className='mt-4 form-control'></textarea>
@@ -38,7 +54,11 @@ export const PostUpdate: React.FC = () => {
         </select>
         <input type="file" {...register("image")} className='mt-4 form-control' />
         {/* <img src={data.image} width={5} /> */}
-          <button className='mt-4 btn btn-success'>Update</button>
+          {mutation.isPending ? (
+            <button className='mt-4 btn btn-secondary'>Updating ....</button>
+          ): (
+            <button className='mt-4 btn btn-success'>Update</button>
+          )}
       </form>
       <Footer/>
     </>
